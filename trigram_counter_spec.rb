6201be70102ac027b3191d0376c90f20571c12d9
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2022-11-14 15:40:56 -0700

    #3 update spec to expect solution without ignoring punctuation and to read fixture file

--- trigram_counter_spec.rb
+++ trigram_counter_spec.rb
@@ -3,12 +3,19 @@ require './trigram_counter'
 
 RSpec.describe TrigramCounter do
   it "returns top 3 most common three-word sequences" do
-    txt_file = './lorem_ipsum_fixture.txt'
+    text = File.read('./lorem_ipsum_fixture.txt')
+    # do i need to close the file?
+
     expected = {
-      "ipsum dolor sit": 5,
-      "dolor sit amet": 5,
-      "sit amet consectetur": 5
+      "ipsum dolor sit"=>5,
+      "dolor sit amet"=>4,
+      "in metus vulputate"=>4
     }
-    expect(TrigramCounter.top_trigrams(txt_file)).to eq(expected)
+
+    expect(TrigramCounter.top_trigrams(text)).to eq(expected)
   end
 end
+
+### future test blocks
+# it catches most frequent sequences regardless of case
+# it catches most frequnent sequences regardless of new lines and punctuation
