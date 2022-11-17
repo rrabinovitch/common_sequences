b5961320958edafef675ab70828c972190fd7a42
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2022-11-17 00:46:40 -0700

    #10 refactor iteration through split_text array to stop looping after 3rd to last word

--- lib/trigram_counter.rb
+++ lib/trigram_counter.rb
@@ -10,16 +10,16 @@ class TrigramCounter
 
     trigram_tracker = Hash.new(0)
 
-    split_text.each_with_index do |word, index|
+    # loop through split_text array until the third to last element
+    for index in (0..split_text.length - 3) do
       # define variable holding current iteration's three-word sequence using index
       three_word_sequence = [split_text[index], split_text[index + 1], split_text[index + 2]].join(' ')
       # check if that sequence exists as a key in trigram_tracker
         # if not, save that variable as a key in trigram_tracker, with value starting at 1
         # if yes, add += 1 to that key's value
       trigram_tracker[three_word_sequence] += 1 # not sure if ||= operator might be a better fit here
-      ### TO DO: stop iteration at third to last word in text
     end
-
+    require "pry"; binding.pry
     # sort trigram_tracker by desc values
     sequences_ordered_by_frequency = trigram_tracker.sort_by { |sequence, frequency| -frequency } # is there a more efficient way of doing this? without needing to transform from hash to array and then back to hash?
     # return top 3
