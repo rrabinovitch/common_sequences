d7cc7243cc8cc51ef377d7fc8e5a40d6703d2c03
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2022-11-18 10:38:47 -0700

    #12 update spec to test only trigram counting functionality, eliminate testing for normalization which has been moved into TextProcessor

--- spec/trigram_counter_spec.rb
+++ spec/trigram_counter_spec.rb
@@ -1,10 +1,15 @@
+# NEXT STEPS:
+### how to update testing now that normalization has been moved into a different class
+### decide whether to keep file reading in test blocks
+### add testing for multiple files
+
 require 'rspec'
 require './lib/trigram_counter'
 
 RSpec.describe TrigramCounter do
   describe "::top_trigrams" do
-    it "returns 100 most common three-word sequences in a single .txt file that does not have any punctuation or newlines to be considered" do
-      text = File.read('./spec/fixtures/normalized_lorem_ipsum_fixture.txt')
+    it "returns 100 most common three-word sequences in a string" do
+      normalized_text = File.read('./spec/fixtures/normalized_lorem_ipsum_fixture.txt').chomp # removing automatic end-of-file newline
 
       top_3_sequences = {
         "dolor sit amet"=>5,
@@ -12,26 +17,10 @@ RSpec.describe TrigramCounter do
         "ipsum dolor sit"=>5
       }
 
-      top_100_sequences = TrigramCounter.top_trigrams(text)
+      top_100_sequences = TrigramCounter.top_trigrams(normalized_text)
 
       expect(top_100_sequences.count).to eq(100)
       expect(top_100_sequences.first(3).to_h).to eq(top_3_sequences)
-    end
-
-    it "returns the same most common three-word sequences in a single .txt file that _does_ have punctuation, newlines, and mixed capitalization" do
-      text = File.read('./spec/fixtures/lorem_ipsum_fixture.txt')
-
-      top_3_sequences = {
-        "dolor sit amet"=>5,
-        "sit amet consectetur"=>5,
-        "ipsum dolor sit"=>5
-      }
-
-      top_100_sequences = TrigramCounter.top_trigrams(text)
-
-      expect(top_100_sequences.count).to eq(100)
-      expect(top_100_sequences.first(3).to_h).to eq(top_3_sequences)
-      # additional testing to consider: confirm that results are appearing in same order across normalized vs denormalized files
     end
   end
 end
