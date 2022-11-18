63a54518e2356cb3fdcb15d32407cd99901517e9
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2022-11-18 10:24:42 -0700

    #12 implement TextProcessor usage in runner file

--- solution.rb
+++ solution.rb
@@ -1,9 +1,8 @@
+require './lib/text_processor'
 require './lib/trigram_counter'
 
-# switch text var from reading file directly to calling on a new TextProcessor class method that returns a string of normalized text
-# can take any number of args and returns a single string of normalized text
-text = File.read(ARGV[0])
-top_trigrams = TrigramCounter.top_trigrams(text)
+normalized_text = TextProcessor.process_text(ARGV)
+top_trigrams = TrigramCounter.top_trigrams(normalized_text)
 top_trigrams.each do |sequence, frequency|
   puts "#{sequence} - #{frequency}"
 end
