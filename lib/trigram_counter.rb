c7d2ab7a13a4aa4a3749feffc54e21aff576c696
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2022-11-18 09:19:04 -0700

    #12 define SEQUENCE_COUNT const and clean up file

--- lib/trigram_counter.rb
+++ lib/trigram_counter.rb
@@ -1,7 +1,8 @@
 class TrigramCounter
+  SEQUENCE_COUNT = 100
+
   def self.top_trigrams(text)
     split_text = normalize_text(text).split
-
     trigram_tracker = Hash.new(0)
 
     for index in (0..split_text.length - 3) do
@@ -9,15 +10,14 @@ class TrigramCounter
       trigram_tracker[three_word_sequence] += 1
     end
 
-    sequences_ordered_by_frequency = trigram_tracker.sort_by { |sequence, frequency| -frequency } # is there a more efficient way of doing this? without needing to transform from hash to array and then back to hash?
-
-    sequences_ordered_by_frequency.first(100).to_h
+    sequences_ordered_by_frequency = trigram_tracker.sort_by { |sequence, frequency| -frequency }
+    sequences_ordered_by_frequency.first(SEQUENCE_COUNT).to_h
   end
 
   def self.normalize_text(text)
-    text.strip.downcase.gsub(/\R+/, ' ').gsub(/[^\w\s]+|_+/i, '').gsub(/-+/, ' ')
-
-    # does not take into account non-word characters like % if they are sandwhiched in between spaces
+    text.strip.downcase.gsub(/\R+/, ' ')
+      .gsub(/[^\w\s]+|_+/i, '')
+      .gsub(/-+/, ' ')
   end
 end
 
